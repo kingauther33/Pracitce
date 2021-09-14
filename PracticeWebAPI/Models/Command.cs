ff67e754440e72c270448fce513dabc95d443ab6
kingauther33 <kingauther33@gmail.com>
2021-09-14 09:46:56 +0700

    Update everything

---
+++ PracticeWebAPI/Models/Command.cs
@@ -0,0 +1,13 @@
+namespace PracticeWebAPI.Models
+{
+  public class Command
+  {
+    public int Id { get; set; }
+
+    public string HowTo { get; set; }
+
+    public string Platform { get; set; }
+
+    public string CommmandLine { get; set; }
+  }
+}
